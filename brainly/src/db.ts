8bbc0ae1d0c4ac376c1c7b6dcf3cda8435f65cab
Mahesh Pawar <mp002995@gmail.com>
2025-05-26 23:59:32 +0530

    add backend url and / route

--- brainly/src/db.ts
+++ brainly/src/db.ts
@@ -1,6 +1,6 @@
 import mongoose, {model, Schema} from "mongoose"
 
-mongoose.connect("mongodb://localhost:27017/brainly")
+mongoose.connect("mongodb+srv://maheshp8767:fDxjzQtFsQ630hc6@cluster0.d9hnqbb.mongodb.net/")
 
 //user schema
 const UserSchema = new Schema({
@@ -15,6 +15,7 @@ export const userModel = model("User", UserSchema)
 const ContentSchema = new Schema({
     title:String,
     link:String,
+    type:String,
     tags:[{type:mongoose.Types.ObjectId, ref:'Tag'}],
     userId:{type:mongoose.Types.ObjectId, ref:"User", required:true}
 })
